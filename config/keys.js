076210747caaeb029e2e669227b4514325501848
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-07-02 15:42:29 -0600

    attempt to fix heroku hosting issue (check keys.js)

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://cruebee:Alycatterson420@travel-cluster.k6ahw.mongodb.net/travel-cluster?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://cruebee:Alycatterson420@travel-cluster.k6ahw.mongodb.net/.items?retryWrites=true&w=majority'
 }
