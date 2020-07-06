d404123e130c54105b75c0c6abfb98d3ab449a80
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-07-06 15:03:31 -0600

    update backend to allow users to register with hashed passwords

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
-
-const items = require('./routes/api/items');
 
 const app = express();
 
 // BodyParser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Connect to MongoDB (local testing)
 // must switch back and fourth between localhost testing and heroku hosting which uses a secure way to transfer passwords
-/*
+
 const db = require('../config/keys').mongoURI;
 
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
-*/
+
 
 // Connect to MongoDB with Heroku hosting
 // Comment out to switch back to local host testing (Note uuid is no longer in use when in testing.)
+/*
 mongoose
   .connect(process.env.CONNECTION_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
+*/
 
 //Use Routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
